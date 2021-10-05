0f57de993599e3c84d735fe1ec65be234479e5a1
Phil G <fillupgalbraith@gmail.com>
2021-10-05 10:50:53 -0700

    first commit

--- index.js
+++ index.js
@@ -109,10 +109,30 @@ Puppies less than 1 year
 NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
+function hungryDog(weight, age){
+  if (age >= 1 ){
+    if(weight <= 5){
+      return 0.05 * weight
+    } else if(weight >= 6 && weight <= 10){
+      return 0.04 * weight
+    } else if(weight >= 11 && weight <= 15){
+      return 0.03 * weight
+    } else {
+      return 0.02 * weight
+    }
+  } else {
+    if (age >= (2/12) && age < (4/12)){
+        return 0.1 * weight
+    } else if(age >= (4/12) && age < (7/12)){
+        return 0.05 * weight
+      } else if(age >= (7/12) && age < 1){
+        return 0.04 * weight
+     
+      }
+  }
   /*add your code here*/
 }
-
+hungryDog()
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
